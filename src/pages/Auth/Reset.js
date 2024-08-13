489abbdfbb0ad18bffeebd5b891f4035608febca
Abel-alx-github <abeltibeso@gmail.com>
2024-08-13 17:15:12 +0300

    impelement reset password on firebase

--- src/pages/Auth/Reset.js
+++ src/pages/Auth/Reset.js
@@ -1,14 +1,41 @@
 import styles from './auth.module.scss';
 import resetImg from  '../../assetes/forgot.png';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Card from '../../components/Card/Card';
-
-
+import Loader from '../../components/Loader/Loader';
+import { auth } from '../../firebaseConfig/firebaseConfig';
+import { toast } from 'react-toastify';
+import { useState } from 'react';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 const Reset = () => {
-      return (
-        <section className={`container ${styles.auth}`}>
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const navigate = useNavigate()
+
+  const handleResetPassword = (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    sendPasswordResetEmail(auth, email)
+    .then( () => {
+      setIsLoading(false)
+      toast.success("We sent an Emaill.\nLook your email")
+      navigate("/login")
+    })
+    .catch((error) => {
+      setIsLoading(false)
+      toast.error(error.message)
+
+    })
+
+  }
+  return (
+       <>
+       { isLoading && <Loader />}
+       <section className={`container ${styles.auth}`}>
             <div className={styles.img}>
                 <img src={resetImg} alt="reset" width="400" />
             </div>
@@ -17,9 +44,14 @@ const Reset = () => {
             <div className={styles.form}>
                  
                     <h2>Reset Password</h2>
-                    <form >
-                        <input type="email" placeholder='Email' required />
-                        <button className="--btn --btn-primary --btn-block">Reset Password</button>
+                    <form onSubmit={handleResetPassword}>
+                        <input type="email"
+                               placeholder='Email'
+                               required
+                               value={email}
+                               onChange={(e) => setEmail(e.target.value)}
+                               />
+                        <button type="submit" className="--btn --btn-primary --btn-block">Reset Password</button>
                     
                         <div className={styles.links}>
                           <p>
@@ -34,6 +66,7 @@ const Reset = () => {
             </div>
         </Card>
         </section>
+       </>
     
       )
 }
