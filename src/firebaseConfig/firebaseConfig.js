dc532b863cea037a4c8506fc4186f3ff22b9ca34
Abel Tibeso <138770113+Abel-alx-github@users.noreply.github.com>
2024-08-14 11:23:44 +0300

    remove secrity apiKey from firebaseConfig.js

--- src/firebaseConfig/firebaseConfig.js
+++ src/firebaseConfig/firebaseConfig.js
@@ -4,12 +4,12 @@ import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCm_LFdbpIqXB7riHJ6V95TZDxk_LEV7E4",
+    apiKey: "apiKey",
     authDomain: "react-zitashop.firebaseapp.com",
-    projectId: "react-zitashop",
-    storageBucket: "react-zitashop.appspot.com",
-    messagingSenderId: "587668674933",
-    appId: "1:587668674933:web:07e01c3a92caa8d5ac1b3c"
+    projectId: "projectId",
+    storageBucket: "storage",
+    messagingSenderId: "messaging",
+    appId: "appId"
   };
   
 // Initialize Firebase
